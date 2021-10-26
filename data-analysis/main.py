719f00abba4b552fda6469c5a8759036ae89a25d
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-10-25 18:02:57 -0700

    x

--- data-analysis/main.py
+++ data-analysis/main.py
@@ -6,6 +6,6 @@ from utils import *
 from normalization import *
 from augmentation import *
 
-data = np.load("./npy-arrays/bat_data1.npy", allow_pickle=True, fix_imports=False)
+data = np.load("./npy-arrays/bat_data1_normalized.npy", allow_pickle=True, fix_imports=False)
 
 augment_data_by_class(data, "19")
